10fc6439641bd955cce0d2aa27c0b529def4d996
jelvs <jelvs13@gmail.com>
2018-09-19 18:26:03 +0100

    Fixes issue with random, generate unique for each thread

--- main.c
+++ main.c
@@ -4,50 +4,50 @@
 #include <unistd.h>
 #include <math.h>
 
-#define RADIUS 1
 
-struct Result{
+struct Result {
     int total;
     int inside;
 };
 
-struct Input{
+struct Input {
     int numberOfShots;
-    struct Result* resultPointer;
+    struct Result *resultPointer;
 };
 
 /* generate a random floating point number from min to max */
-double fRand()
-{
-   return (double)rand() / RAND_MAX;
+double fRand(unsigned int *unique) {
+    return (double) rand_r(unique) / RAND_MAX;
 
 }
 
-double calculateDistanceToPoint (double x, double y) {
+double calculateDistanceToPoint(double x, double y) {
     return sqrt((x * x) + (y * y));
 }
 
-void writeTotal (int total1, int inside1, struct Result* rst) {
+void writeTotal(int total1, int inside1, struct Result *rst) {
     rst->total = total1;
     rst->inside = inside1;
 
 }
 
-void* approxPi (void *args) {
 
-    struct Input*  input = (struct Input*) args;
+void *approxPi(void *args) {
+
+    struct Input *input = (struct Input *) args;
     int numberOfShots = input->numberOfShots;
 
 
     int total = 0;
     int inside = 0;
 
-    srand(getpid());
+    unsigned int state = time(NULL) ^ getpid() ^ (int)pthread_self();
+
 
     int i;
     for (i = 0; i < numberOfShots; i++) {
-        double xCoor = fRand();
-        double yCoor = fRand();
+        double xCoor = fRand(&state);
+        double yCoor = fRand(&state);
 
         double distance = calculateDistanceToPoint(xCoor, yCoor);
         total++;
@@ -67,7 +67,8 @@ int main(int argc, char **argv) {
     int numberOfTries;
     int numberOfThreads;
 
-    if(argc != 3) {
+
+    if (argc != 3) {
         printf("Usage: $ ApproxPi numberOfShots numberOfThreads\n");
         return 1;
     }
@@ -75,14 +76,14 @@ int main(int argc, char **argv) {
     numberOfTries = strtol(argv[1], NULL, 0);
     numberOfThreads = strtol(argv[2], NULL, 0);
 
-    int  total = 0;
+    int total = 0;
     int inside = 0;
     int shotsByThread = numberOfTries / numberOfThreads;
 
     struct Input inputs[numberOfThreads];
 
 
-    struct Result rst [numberOfThreads];
+    struct Result rst[numberOfThreads];
 
     pthread_t tid[numberOfThreads];
     for (int i = 0; i < numberOfThreads; i++) {
@@ -90,24 +91,24 @@ int main(int argc, char **argv) {
         inputs[i].numberOfShots = shotsByThread;
         inputs[i].resultPointer = &rst[i];
 
-        void* args = (void*) &inputs[i];
+        void *args = (void *) &inputs[i];
 
         pthread_create(&tid[i], NULL, approxPi, args);
     }
-    for (int i = 0; i<numberOfThreads; i++)
+    for (int i = 0; i < numberOfThreads; i++)
         pthread_join(tid[i], NULL);
 
-    for (int i = 0; i<numberOfThreads; i++){
-        total+= rst[i].total;
-        inside+=rst[i].inside;
+    for (int i = 0; i < numberOfThreads; i++) {
+        total += rst[i].total;
+        inside += rst[i].inside;
 
-    }if(total != numberOfTries){
+    }
+    if (total != numberOfTries) {
         printf("The threads did not perform all of the shots\n");
     }
 
 
-
-    double pi = ((double)inside/(double)total)*4;
+    double pi = ((double) inside / (double) total) * 4;
     printf("Approximation of Pi: %f\n", pi);
 
     return 0;
